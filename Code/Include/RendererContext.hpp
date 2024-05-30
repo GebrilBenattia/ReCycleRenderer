72533aa42d69b67e0fd76186ff11eea72b943530
GebrilBenattia <benattia.gebril974@gmail.com>
2024-05-30 22:19:27 +0200

    Adding RendererContexts TODO make Vulkan's RendererContext

---
+++ Code/Include/RendererContext.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+class RendererContext
+{
+private:
+
+public:
+
+};
+
+class OpenGLContext : public RendererContext
+{
+private:
+
+public:
+
+};
+
+class VulkanContext : public RendererContext
+{
+private:
+
+public:
+
+};
+
+class DirectXContext : public RendererContext
+{
+private:
+
+public:
+
+};
