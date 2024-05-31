a6664953486e0c170ed94e489fb89e8c9b7e094a
g.benattia <g.benattia@student.isartdigital.com>
2024-05-31 13:59:13 +0200

    Adding Lauching and Shutdown print, so that even in Release we can know in the command prompt at which stage we are (I'll add more later)

--- Code/Source/Application.cpp
+++ Code/Source/Application.cpp
@@ -5,6 +5,7 @@ Application::Application()
 {
 	r = new Renderer();
 
+	std::cout << "\x1b[32m" << "Launching ReCycle..." << "\x1b[0m" << std::endl;
 	Init();
 }
 
@@ -25,5 +26,5 @@ void Application::Update()
 
 void Application::Shutdown()
 {
-
+	std::cout << "\x1b[31m" << "Closing ReCycle..." << "\x1b[0m" << std::endl;
 }
